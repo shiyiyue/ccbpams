c838374fe8d531052b6e90d5c0998462d16d1122
zhanrui <zhanrui@gmail.com>
2015-06-07 12:33:22 +0800

    资金流向

---
+++ main/java/pams/repo4odsbpbs/dao/common/AssetImportMapper.java
@@ -0,0 +1,14 @@
+package pams.repo4odsbpbs.dao.common;
+
+import org.springframework.stereotype.Component;
+import pams.repo4odsbpbs.model.QdbgGjCustInfoAll;
+import pams.repo4odsbpbs.model.common.AssetQryParam;
+
+import java.util.List;
+
+@Component
+public interface AssetImportMapper {
+    int countRecords(AssetQryParam paramBean);
+    List<QdbgGjCustInfoAll> selectPagedRecords(AssetQryParam paramBean);
+    List<QdbgGjCustInfoAll> selectRecords(AssetQryParam paramBean);
+}
